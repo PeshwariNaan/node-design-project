895740b5cbaeb071259bc824d2e66c17f153cec4
J.D. Browne <PeshwariNaan01@proton.me>
2023-01-13 16:59:26 +0100

    Added error handler for unmatched routes.

--- app.js
+++ app.js
@@ -31,4 +31,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+//Middleware for error handling all routes that don't match the two above
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
+
 module.exports = app;
