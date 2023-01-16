ce22fe73394217d0e44409dc16730e09c1014f5c
J.D. Browne <PeshwariNaan01@proton.me>
2023-01-16 16:31:47 +0100

    Modified error controller to have prod and dev differences. Added check for tour in controllers that use ID.

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,40 @@
+const sendErrorForDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    err: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorForProduction = (err, res) => {
+  //Operational error that we trust: Send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming or other unknown error: Don't leak error details
+  } else {
+    // 1) Log error
+    console.error('ERROR  🔥', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'developement') {
+    sendErrorForDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorForProduction(err, res);
+  }
 };
