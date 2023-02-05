5b9729700669f752bd9cec63f829fcc2a8bb393a
J.D. Browne <PeshwariNaan01@proton.me>
2023-02-05 11:41:38 +0100

    Added new review routes to app.js.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
@@ -78,6 +79,7 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 //Middleware for error handling all routes that don't match the two above
 app.all('*', (req, res, next) => {
