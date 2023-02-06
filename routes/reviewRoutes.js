fbbc920fa05ad731e3f0099a217f6b38d204d153
J.D. Browne <PeshwariNaan01@proton.me>
2023-02-06 11:43:09 +0100

    Added review routes - incorporated nested routes.

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true }); // This gives us access to the tour id from the tour router
 
 router
   .route('/')
