26c06167595413b1de995ea54c59ca287934dcf1
J.D. Browne <PeshwariNaan01@proton.me>
2023-02-06 16:27:50 +0100

    Modified all routes for protect and auth.

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,11 +4,11 @@ const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true }); // This gives us access to the tour id from the tour router
 
+router.use(authController.protect);
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourAndUserId,
     reviewController.createReview
@@ -17,7 +17,13 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview);
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
+  );
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,25 +6,19 @@ const router = express.Router();
 
 router.post('/signup', authController.signup); //We only need post for this route to signup a new user
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
+// With this here - all routes after are now protected because of how middleware runs in sequence
+router.use(authController.protect);
 
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.patch('/updateMyPassword', authController.updatePassword);
 
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
 router
   .route('/')
   .get(userController.getAllUsers)
