d181a715776e37bcedc104fe5599007a67b28785
J.D. Browne <PeshwariNaan01@proton.me>
2023-02-06 11:42:04 +0100

    Added reviewController.

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,7 +2,9 @@ const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+  const reviews = await Review.find(filter);
 
   res.status(200).json({
     status: 'success',
@@ -14,11 +16,13 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  // Allow nested route - The code below
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id; //We get the user id from the protect middleware
   const newReview = await Review.create(req.body);
 
   res.status(200).json({
     status: 'success',
-
     data: {
       reviews: newReview,
     },
