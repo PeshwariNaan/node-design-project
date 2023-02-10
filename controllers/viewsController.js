e44cd521a21b68db9e512e4fea6ef0b0b06ac85e
J.D. Browne <PeshwariNaan01@proton.me>
2023-02-10 12:47:25 +0100

    Modified viewRoutes and viewController for tour page.

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -14,10 +14,17 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  //This is how we set things up and node automatically knows to use base
-  res.status(200).render('tour', {
-    tour: 'The Forest Hiker',
-    user: 'Josh',
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) Get the data for the requested tour including reviews and guides
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+  // 2) Build template
+
+  // 3) Render the template using data from step 1
+  res.status(200).render('tour', {
+    title: 'Not sure what this should be',
+    tour,
+  });
+});
