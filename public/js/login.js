3ab27ae91c78682befb496882da3aeb1c7c8c54b
J.D. Browne <PeshwariNaan01@proton.me>
2023-02-12 13:00:13 +0100

    Added login.js file to use axios to make a post request to log user in.

---
+++ public/js/login.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+
+const login = async (email, password) => {
+  console.log(email, password);
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      data: {
+        email,
+        password,
+      },
+    });
+    console.log('Result: ', res);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+document.querySelector('.form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  login(email, password);
+});
