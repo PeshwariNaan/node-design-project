0495eca94af9e07a610d4cb560d72c91a559dc8d
J.D. Browne <PeshwariNaan01@proton.me>
2023-02-12 13:02:24 +0100

    Added controller to login user

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -4,9 +4,7 @@ const catchAsync = require('../utils/catchAsync');
 exports.getOverview = catchAsync(async (req, res) => {
   // 1) Get all tour data from collection
   const tours = await Tour.find();
-
   // 2) Build template
-
   // 3) Render the template using data from step 1
   res.status(200).render('overview', {
     title: 'All Tours',
@@ -14,17 +12,28 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1) Get the data for the requested tour including reviews and guides
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user',
   });
   // 2) Build template
-
   // 3) Render the template using data from step 1
-  res.status(200).render('tour', {
-    title: 'Not sure what this should be',
-    tour,
-  });
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      'connect-src https://*.tiles.mapbox.com https://api.mapbox.com https://events.mapbox.com'
+    )
+    .render('tour', {
+      title: `${tour.name} Tour`,
+      tour,
+    });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account',
+  });
+};
