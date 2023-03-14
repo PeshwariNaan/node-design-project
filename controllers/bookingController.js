8518114d2f8a7d1a3cf807e3123c81586be01e2e
J.D. Browne <PeshwariNaan01@proton.me>
2023-03-14 14:45:36 +0100

    Fixing route in booking controller.

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -26,7 +26,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${
+                tour.imageCover
+              }`,
+            ],
           },
           unit_amount: tour.price * 100,
         },
